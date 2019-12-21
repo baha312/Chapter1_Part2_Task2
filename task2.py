e2374aceea4db79f493971fd4c793c34fef5e29a
baha312 <bahtiyarbolotbekov@gmail.com>
2019-12-21 11:40:41 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,8 @@
+N = int(input("Enter the number of students: "))
+K = int(input("Enter the number of apples: "))
+
+z = K/N # each student will get apples
+y = K%N # OSTATOK APPLES
+
+print(z)
+print(y)
